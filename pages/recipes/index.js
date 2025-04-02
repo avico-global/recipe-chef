62419c9de7a75abc99481b5e5cfa2a1cfc18f925
Usama <usamamustafa393@gmail.com>
2025-04-02 12:06:05 +0500

    update id page

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -482,10 +482,9 @@ export default function RecipesList() {
                 <h3 className="text-lg font-medium text-gray-900 mb-2">
                   No recipes found
                 </h3>
-                <p className="text-gray-600">
-                  Try adjusting your search or filter criteria to find what
-                  you're looking for.
-                </p>
+                <span className="text-sm text-gray-500">
+                  Can&apos;t find what you&apos;re looking for?
+                </span>
               </div>
             ) : (
               <>
